bf586e992bc77a81711bc8ba25b5a518529e64ff
EARL-MILLEN\Millen <earlmillen7@gmail.com>
2025-11-01 13:27:14 +0000

    modified:   func.js

--- func.js
+++ func.js
@@ -113,7 +113,7 @@ function renderCards() {
 // Initial render after data should be loaded (gives fetch a moment)
 setTimeout(() => {
   renderCards();
-}, 5000);
+}, 500);
 
 const buttonGroup = document.querySelector(".btnList");
 const buttons = buttonGroup.querySelectorAll(".menu-button");
